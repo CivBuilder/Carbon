ae0a6d05f6510828ab3ab303a0a57e246c06a383
SnellJ2 <112909707+SnellJ2@users.noreply.github.com>
2023-01-24 13:45:17 -0800

    Update Jonas-Snell_CollectFiles.py

--- repo_mining/Jonas-Snell_CollectFiles.py
+++ repo_mining/Jonas-Snell_CollectFiles.py
@@ -70,7 +70,7 @@ repo = 'scottyab/rootbeer'
 # Remember to empty the list when going to commit to GitHub.
 # Otherwise they will all be reverted and you will have to re-create them
 # I would advise to create more than one token for repos with heavy commits
-lstTokens = ["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"]
+lstTokens = [""]
 
 dictfiles = dict()
 countfiles(dictfiles, lstTokens, repo)
