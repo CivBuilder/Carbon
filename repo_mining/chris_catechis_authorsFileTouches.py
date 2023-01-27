6b1e5cbcf195f6fde625e04f570fbd90c4b65448
Chris Catechis <catecc1@unlv.nevada.edu>
2023-01-26 23:49:42 -0800

    add collect files

--- repo_mining/chris_catechis_authorsFileTouches.py
+++ repo_mining/chris_catechis_authorsFileTouches.py
@@ -2,13 +2,13 @@ import json
 # importing this way below as VS code errors without pip._vendor
 import requests
 import csv
-
 import os
 
 if not os.path.exists("data"):
- os.makedirs("data")
+    os.makedirs("data")
 
 # GitHub Authentication function
+
 def github_auth(url, lsttoken, ct):
     jsonData = None
     try:
@@ -25,7 +25,9 @@ def github_auth(url, lsttoken, ct):
 # @dictFiles, empty dictionary of files
 # @lstTokens, GitHub authentication tokens
 # @repo, GitHub repo
+
 def countfiles(dictfiles, lsttokens, repo):
+    languages = ['.java', '.js']  # filetypes to read in
     ipage = 1  # url page counter
     ct = 0  # token counter
 
@@ -33,7 +35,8 @@ def countfiles(dictfiles, lsttokens, repo):
         # loop though all the commit pages until the last returned empty page
         while True:
             spage = str(ipage)
-            commitsUrl = 'https://api.github.com/repos/' + repo + '/commits?page=' + spage + '&per_page=100'
+            commitsUrl = 'https://api.github.com/repos/' + \
+                repo + '/commits?page=' + spage + '&per_page=100'
             jsonCommits, ct = github_auth(commitsUrl, lsttokens, ct)
 
             # break out of the while loop if there are no more commits in the pages
@@ -48,12 +51,22 @@ def countfiles(dictfiles, lsttokens, repo):
                 filesjson = shaDetails['files']
                 for filenameObj in filesjson:
                     filename = filenameObj['filename']
-                    dictfiles[filename] = dictfiles.get(filename, 0) + 1
-                    print(filename)
+                    for languageExt in languages:
+                        if languageExt in filename:
+                            author = shaObject['commit']['author']['name']
+                            date = shaObject['commit']['author']['date']
+                            fileInfo = [author, date]
+                            dictfiles[filename] = []
+                            dictfiles[filename].extend(fileInfo)
+                        else:
+                            print('not valid extension')
+                            continue
             ipage += 1
     except:
         print("Error receiving data")
         exit(0)
+
+
 # GitHub repo
 repo = 'scottyab/rootbeer'
 # repo = 'Skyscanner/backpack' # This repo is commit heavy. It takes long to finish executing
@@ -72,8 +85,8 @@ print('Total number of files: ' + str(len(dictfiles)))
 
 file = repo.split('/')[1]
 # change this to the path of your file
-fileOutput = 'data/file_' + file + '.csv'
-rows = ["Filename", "Touches"]
+fileOutput = 'data/file_chris_data' + file + '.csv'
+rows = ["Filename", "Fileinfo"]
 fileCSV = open(fileOutput, 'w')
 writer = csv.writer(fileCSV)
 writer.writerow(rows)
