22f54c7c9d7e3823e6e554b85fe375cac977127c
Chris Catechis <catecc1@unlv.nevada.edu>
2023-01-27 22:49:03 -0800

    Update chris_catechis_scatterplot.py

--- repo_mining/chris_catechis_scatterplot.py
+++ repo_mining/chris_catechis_scatterplot.py
@@ -6,7 +6,7 @@ import json
 import csv
 
 istream = []
-authors = []
+files = []
 dates = []
 x_axis = []
 y_axis = []
@@ -19,23 +19,27 @@ with open('repo_mining/data/file_chris_datarootbeer.csv', 'r') as csvfile:
         istream.append(dict(row))
 
 # pasrse csv
-filenames = [x['Filename'] for x in istream]
+authors = [x['Author'] for x in istream]
 fileinfo = [y['Fileinfo'] for y in istream]
 
 # parse fileinfo
 for entry in fileinfo:
     # remove unneccessary chars
+    current_color = (np.random.random(), np.random.random(), np.random.random())
     for char in replaceChars:
         entry = entry.replace(char, '')
     # parse data into arrays
     info = entry.split(',')
-    authors.append(info[0])
-    info[1] = info[1].strip()
-    dates.append(info[1])
+    for data in info:
+        if (info.index(data) % 2 == 0):
+            files.append(data)
+            colors.append(current_color)
+        else:
+            data = data.strip()
+            dates.append(data)
 
 # clean data for plotting
 today = datetime.now()
-print(today)
 for date in dates:
     current = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
     if dates.index(date) == 0:
@@ -46,15 +50,12 @@ for date in dates:
     current = ((today - current).days / 7)
     y_axis.append(current)
 
-for author in authors:
-        current_color = (random.random(), random.random(), random.random())
-        colors.append(current_color)
+for file in files:
+    x_axis.append(files.index(file))
 
-for file in filenames:
-    x_axis.append(filenames.index(file))
 
 plt.title('File Touches by Author')
 plt.xlabel('File')
 plt.ylabel('Weeks')
-plt.scatter(x_axis, y_axis, s=75, c=colors)
+plt.scatter(x_axis, y_axis, s=30, c=colors)
 plt.show()
