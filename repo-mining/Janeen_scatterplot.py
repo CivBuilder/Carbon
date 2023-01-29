b85a95d919659e2d0abcf168d2366a6ab07ca0d9
Janeen Sudiacal <janeen.sudiacal@gmail.com>
2023-01-29 14:53:08 -0800

    Delete repo-mining directory

--- repo-mining/Janeen_scatterplot.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("data/file_authors.csv")
-plt.scatter(df['FileNumber'], df['Month'], c=df['AuthorNumber'])
-plt.xlabel("File")
-plt.ylabel("Month")
-plt.show()
