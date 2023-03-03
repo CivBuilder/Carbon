b9471ea8dfa3deb10698ab7323b0816b161b8208
Erik <erikr.ramsey@gmail.com>
2023-03-03 05:18:17 -0800

    Added content to forumScreen - fetches database content from /api/forumcontent - renders each piece separately in renderContent - added view around category selection

--- Carbon/navigation/screens/ForumScreen.js
+++ Carbon/navigation/screens/ForumScreen.js
@@ -1,95 +1,101 @@
 import * as React from 'react';
-import { View, Text, ScrollView, StyleSheet} from 'react-native';
+import { View, Text, ScrollView, StyleSheet, FlatList} from 'react-native';
 import {useState, useEffect} from 'react';
 import EducationMenu from '../../components/EducationMenu';
 
-
+const APIURL = "http://carbontest-env.eba-gpgdvm85.us-west-1.elasticbeanstalk.com/api"
+// Using my test AWS node server for now
 export default function ForumScreen({navigation}) {
-    const getData = async() => {
-        try {
-            console.debug("fetching forum content");
-            await fetch('http://192.168.0.150:3000/forumcontent')
-                .then(response => response.json())
-                .then(json => {
-                    console.log(json);
-                });
-        } catch (error) {
-            console.error(error);
-        }
+    const [forumData, setForumData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    // For now we fetch all of it, have to make an endpoint that allows fetching by category
+    const fetchData = async() => {
+        console.log("Fetching data for forumcontent");
+        const response = await fetch(APIURL + "/forumcontent");
+        const data = await response.json();
+        setForumData(data.content);
+        setLoading(false);
+    };
+
+    const renderContent = ({item}) => {
+        return(
+            <View
+                style = {{
+                    borderRadius: 10,
+                    backgroundColor: '#74C69D',
+                    paddingHorizontal : 14,
+                    paddingVertical: 90,
+                    margin: 5,
+                    width : '45%'
+                }}
+            >
+                <Text style= {{textAlign : 'center'}}>{item.type}</Text>
+            </View>
+        )
     }
 
     useEffect(() => {
-        getData();
+        fetchData();
     }, []);
 
-   const [type, setType] = useState("food");
-   
-   const styles = StyleSheet.create({
+    const [type, setType] = useState("food");
+    const styles = StyleSheet.create({
        row: {
            flexDirection:'row'
        }
-   })
-   return(
+    })
 
-   //     default stuff
-   //     <View
-   //         style={{
-   //             flex: 1,
-   //             alignItems: 'center',
-   //             justifyContent: 'center'
-   //         }}
-   //     >
-   //         <Text
-   //             onPress={() =>
-   //                 navigation.navigate('Home')
-   //             }
-   //             style={{
-   //                 fontSize: 26,
-   //                 fontWeight: 'bold'
-   //             }}
-   //         >
-   //             Forum Screen
-   //         </Text>
-   //     </View>
+    return(
+        <View>
+            <View>
+                <ScrollView>
+                    <View style ={{flex: 3, flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 50, marginBottom: 75}}>
+                        <EducationMenu
+                            key={"food"}
+                            title = "Food"
+                                type ={type}
+                                setType = {setType}
+                        />
+                        <EducationMenu
+                                key={"transportation"}
+                                title = "Transportation"
+                                type ={type}
+                                setType = {setType}
+                        />
+                        <EducationMenu
+                            key={"recycling"}
+                            title = "Recycling"
+                                type ={type}
+                                setType = {setType}
+                        />
+                        <EducationMenu
+                            key={"water"}
+                            title = "Water"
+                                type ={type}
+                                setType = {setType}
+                        />
+                        <EducationMenu
+                            key={"electricity"}
+                            title = "Electricity"
 
-
-       <ScrollView>
-           <View style ={{flex: 3, flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 50 }}>
-               <EducationMenu
-                   key={"food"}
-                   title = "Food"
-
-                    type ={type}
-                    setType = {setType}
-                />
-                <EducationMenu
-                    key={"transportation"}
-                    title = "Transportation"
-                    type ={type}
-                    setType = {setType}
-
-               />
-               <EducationMenu
-                   key={"recycling"}
-                   title = "Recycling"
-                    type ={type}
-                    setType = {setType}
-               />
-               <EducationMenu
-                   key={"water"}
-                   title = "Water"
-                    type ={type}
-                    setType = {setType}
-               />
-               <EducationMenu
-                   key={"electricity"}
-                   title = "Electricity"
-
-                    type ={type}
-                    setType = {setType}
+                            type ={type}
+                            setType = {setType}
+                        />
+                    </View>
+                </ScrollView>
+            </View>
+            <View>
+                {loading && <Text>Loading</Text>}
+                <FlatList
+                    horizontal={false}
+                    numColumns = {2}
+                    data={forumData}
+                    renderItem = {renderContent}
+                    keyExtractor = {(item) => item.id_forumcontent.toString()}
                 />
             </View>
-        </ScrollView>
+        </View>
     )
 }
 
