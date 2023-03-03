c173b18b05b021319bd9160508e4f8cebf95ad67
Erik <erikr.ramsey@gmail.com>
2023-03-02 20:26:57 -0800

    Removing server files -theyre temporary and subject to change

--- server/utils/database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-// Local host, once we have a real server do not include these on public github !!!!
-const sequelize = new Sequelize('crbn', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-export default sequelize;
