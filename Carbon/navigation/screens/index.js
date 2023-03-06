cccd046296dcd3f8811ae289f65130744227979d
Miguel Baniqued <bmiguelcedric@gmail.com>
2023-03-05 22:45:16 -0800

    Added UI elements for Progress Screen
    - Created a header for progress screen
    - Created a plus button inside the header
    - Created a separate page where data will get inputted. This page is linked to the plus button
    - Fixed margins for header titles
    - Increased font size for header titles

--- Carbon/navigation/screens/index.js
+++ Carbon/navigation/screens/index.js
@@ -3,3 +3,4 @@ export {default as ProgressScreen} from './Progress/ProgressScreen';
 export {default as ForumScreen} from './Forum/ForumScreen';
 export {default as RankingScreen} from './Ranking/RankingScreen';
 export {default as SettingsScreen} from './Settings/SettingsScreen';
+export {default as AddProgress} from './Progress/AddProgress';
