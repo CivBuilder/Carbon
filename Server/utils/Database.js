91ac35a9b6d4c488bcbd9f8a90fda304b25963e5
Erik <erikr.ramsey@gmail.com>
2023-03-06 17:24:54 -0800

    oopsie

--- Server/utils/Database.js
+++ Server/utils/Database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 // Local host, once we have a real server do not include these on public github !!!!
-const sequelize = new Sequelize('carbon_test', 'carbonDev', 'carbonRules123!', {
-    host: 'carbon-test.cnyblmtbcmm5.us-west-1.rds.amazonaws.com',
+const sequelize = new Sequelize('crbn', 'carbonDev', 'carbonRules123!', {
+    host: 'carbondb-rds.caxzpxqes9ki.us-east-1.rds.amazonaws.com',
     dialect: 'mysql'
 });
 
