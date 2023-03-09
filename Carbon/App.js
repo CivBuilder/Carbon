e478ad8e24587a796632669430d8c0e2b880f871
erickserr125 <erickserr125@gmail.com>
2023-03-09 14:14:04 -0800

    moved nav container to app.js for nesting nav containers

--- Carbon/App.js
+++ Carbon/App.js
@@ -1,14 +1,25 @@
 import * as React from 'react';
 import MainContainer from './navigation/MainContainer';
+import QuestionnaireContainer from './navigation/QuestionnaireContainer';
+//import { Amplify } from 'aws-amplify'
+//import awsconfig from './src/aws-exports'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+//Amplify.configure(awsconfig)
 
-import { Amplify } from 'aws-amplify'
-import awsconfig from './src/aws-exports'
-Amplify.configure(awsconfig)
 
+const Stack = createNativeStackNavigator();
 function App(){
   return (
-    <MainContainer /> //links to MainContainer.js in navigation folder
-  )
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Questionnaire">
+        <Stack.Screen name ="Questionnaire"
+        component ={QuestionnaireContainer} />
+        <Stack.Screen name = "Home"
+        component = {MainContainer}/>
+    </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
