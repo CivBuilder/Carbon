dcd05d40dc876ab400fe171a5675dabe7c484ceb
Chris Catechis <catecc1@unlv.nevada.edu>
2023-03-09 20:37:45 -0800

    update button for set goal

--- Carbon/navigation/screens/Progress/ProgressScreen.js
+++ Carbon/navigation/screens/Progress/ProgressScreen.js
@@ -8,30 +8,30 @@ const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 const margin = 10;
 
-export default function ProgressScreen({navigation}) {
-    return(
-        <SafeAreaView style={{height: windowHeight}}>
+export default function ProgressScreen({ navigation }) {
+    return (
+        <SafeAreaView>
             <ScrollView
                 showsHorizontalScrollIndicator={false}
-                style={{flexGrow: 1}}
+                style={{ flexGrow: 1 }}
             >
                 {/* Chart #1 */}
                 <View style={styles.container}>
                     <Text style={styles.headerTitle}>By Time</Text>
-                    <LineChartFootprint/>
+                    <LineChartFootprint />
                 </View>
 
                 {/* Chart #2 */}
                 <View style={styles.container}>
                     <Text style={styles.headerTitle}>By Category</Text>
-                    <View style={{backgroundColor: Colors.primary.MINT_CREAM, borderRadius: 16}}>
-                        <CatgegoryChartv2/>
+                    <View style={{ backgroundColor: Colors.primary.MINT_CREAM, borderRadius: 16 }}>
+                        <CatgegoryChartv2 />
                     </View>
                 </View>
                 <View>
-                    <TouchableOpacity onPress={() => navigation.navigate(ScreenNames.ADD_GOAL)} style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                        <View style={{backgroundColor: Colors.primary.MINT, height: 40, width: 80, alignItems: 'center', justifyContent: 'center'}}>
-                            <Text style={{textAlign: 'center'}}>Add Goal</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate(ScreenNames.ADD_GOAL)} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Set Goal</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
@@ -49,5 +49,18 @@ const styles = StyleSheet.create({
     container: {
         margin: margin,
         // backgroundColor: "lightblue",
+    },
+    button: {
+        backgroundColor: Colors.primary.MINT,
+        height: 40,
+        width: 105,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 16,
+    },
+    buttonText: {
+        color: Colors.primary.MINT_CREAM,
+        fontSize: 20,
+        fontWeight: 'bold',
     },
 });
