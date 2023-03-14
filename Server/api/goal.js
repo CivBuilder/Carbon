cdaf02003e726977c88e0859126420c08340b9fc
Chris Catechis <catecc1@unlv.nevada.edu>
2023-03-13 21:06:43 -0700

    refactor endpoint
    refactor endpoint to be more robust

--- Server/api/goal.js
+++ Server/api/goal.js
@@ -1,14 +1,25 @@
 const express = require('express');
-let router = express.Router();
-let User = require('../models/User.js');
+const router = express.Router();
+const User = require('../models/User');
 
 // put goal in user table in db
-router.put('/', async function (req, res) {
-  const goal = req.body.goal;
+router.put('/', async (req, res) => {
+  let { goal } = req.body;
 
-  await User.update({ goal: goal }, { where: { id: 1 } });
+  try {
+    const user = await User.findOne({ where: { id: 1 } });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
 
-  res.send(200);
+    await user.update({ goal });
+    res.status(200).json({
+      'content': 'Goal updated'
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
 module.exports = router;
