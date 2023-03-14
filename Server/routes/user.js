5c6178869c847cf5c4ff9802de7246e812f83666
Angel ,':) <aq6476@gmail.com>
2023-03-14 14:21:13 -0700

    Frontend Request for Ranking by ID Implemented

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var user_table = require('../models/user.js');
+const sequelize = require('../utils/Database.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:id', async function(req, res, next) {
-    console.log('ayo ,:_)')
+    
     const user_entry = await user_table.findOne({
         where : {
             id : req.params.id
@@ -19,9 +20,27 @@ router.get('/:id', async function(req, res, next) {
         return res.status(404).send('404 : user with ${ID} not found');
     }
     res.status(200).json({
-        user_entry  //get 
+        user_entry  
     });
 });
 
+/* GET User Rank */
+router.get('/rank/:id', async function(req, res, next) {
+    const rank = await user_table.findOne({
+        where : {
+            id : req.params.id
+        },
+        attributes : [
+            [sequelize.literal('(SELECT COUNT(*) FROM user as user2 WHERE user2.global_score > user.global_score) + 1'), 'ranking']
+        ]
+    });
+    if(!rank) {
+        console.log("Sending error code 404. No match found");
+        return res.status(404).send('404 : user with ${ID} not found');
+    }
+    res.status(200).json({
+        rank
+    })
+})
 
 module.exports = router;
