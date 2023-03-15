8a9e3e9a10d6b2e61ddf208db3482657861f4979
Miguel Baniqued <bmiguelcedric@gmail.com>
2023-03-14 18:35:39 -0700

    Uninstalled aws-amplify dependency and any related files

--- Carbon/App.js
+++ Carbon/App.js
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import MainContainer from './navigation/screens/Main/MainContainer';
 
-// import { Amplify } from 'aws-amplify'
-// import awsconfig from './src/aws-exports'
-// Amplify.configure(awsconfig)
-
 function App(){
   return (
     <MainContainer /> //links to MainContainer.js in navigation folder
