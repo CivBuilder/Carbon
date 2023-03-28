79b4a31d39819bc6e754f0aac813ed7f15922d1d
Adam Velardi <114274638+adamantisaurus@users.noreply.github.com>
2023-03-27 20:03:46 -0700

    Minor fixes
    Minor bug fix and updated package to correct version.

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,6 +3,6 @@ export {default as ProgressScreen} from './ProgressScreen';
 export {default as ForumScreen} from './ForumScreen';
 export {default as RankingScreen} from './RankingScreen';
 export {default as SettingsScreen} from './SettingsScreen';
-export {default as BrowserScreen} from './Browser/BrowserScreen';
+export {default as BrowserScreen} from './BrowserScreen';
 export {default as QuizScreen} from './QuizScreen';
 // export {default as AddProgress} from './Progress/AddProgress';
