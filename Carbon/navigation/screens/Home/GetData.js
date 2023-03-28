0c8bf3bdd37b08706f317df62eb21cbb81f9db06
SnellJ2 <112909707+SnellJ2@users.noreply.github.com>
2023-03-28 15:10:10 -0700

    This is a new branch with a bunch of other commits, each one is described below
    Need to add more filtering to only return certain things (The numbers needed to load in the log)
    
    This took a lot of work since I had to mess around with asynchronous functions to make react wait properly.
    
    Parsed the date as well as fixed my state hooks. Made the data returned from the server be of the right type.
    
    Still need to parse the data and put it into arrays.
    
    Parsed all the necessary data from the server. It does quite a bit but in summary we compare dates and return the right values for the 2d array based on that.
    
    Added error check in log.js

--- Carbon/navigation/screens/Home/GetData.js
+++ Carbon/navigation/screens/Home/GetData.js
@@ -5,18 +5,19 @@
 */
 const GetData = async() => {
   try {
-      const url = 'Commit data for ';
+      const user_id = 323;
+
+      const url = `http://192.168.0.6:3000/api/userEmissions?user_id=${user_id}`; //http:// + local ipv4 + :3000/api/useremission to connect
       console.log("fetching data from " + url); //log to see if if it works
       const response = await fetch(url); //wait for response
       const data = await response.json(); //get the data we need
-      console.log(data); //print data for testing
-
-
+      console.log(data);
+      return data
     }
     catch (error) { 
-      console.log(error) //error check
+      console.log(error); //error check
     }
-  
+
 };
   
   
