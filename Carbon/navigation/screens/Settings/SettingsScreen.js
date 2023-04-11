2a990248ea2b05040b287015b70bcf23b4161dec
Chris Catechis <catecc1@unlv.nevada.edu>
2023-04-10 21:24:20 -0700

    initial iteration, add change username and change password

--- Carbon/navigation/screens/Settings/SettingsScreen.js
+++ Carbon/navigation/screens/Settings/SettingsScreen.js
@@ -1,9 +1,17 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import UsernameInput from '../../../components/UsernameInput';
+import PasswordInput from '../../../components/PasswordInput';
+import { Colors } from '../../../colors/Colors';
 import { logout } from '../../../util/LoginManager';
 
-export default function SettingsScreen({navigation}) {
-    return(
+const NonBreakingSpace = () => <Text>{'\u00A0'}</Text>;
+
+const SettingsScreen = ({ navigation }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
         <View
             style={{
                 flex: 1,
@@ -11,7 +19,7 @@ export default function SettingsScreen({navigation}) {
                 justifyContent: 'center'
             }}
         >
-            <Text
+            {/* <Text
                 onPress={() =>
                     navigation.navigate('Home')
                 }
@@ -19,10 +27,33 @@ export default function SettingsScreen({navigation}) {
                     fontSize: 26,
                     fontWeight: 'bold'
                 }}
-            >
-                Settings Screen
-            </Text>
-            <Button title='logout' onPress={() => {logout()}}/>
+            >           </Text> */}
+            {/* Settings */}
+
+            {/* Change username */}
+            <Text style={styles.generalText}>Change username</Text>
+            <UsernameInput testID="usernameInput" onChangeText={un => setUsername(un)} />
+            {/* add a button with an api call to change the username */}
+            <NonBreakingSpace />
+
+            {/* Change password */}
+            <Text style={styles.generalText}>Change password</Text>
+            {/* Change below line to use new api call to check if old password matches */}
+            <PasswordInput text="Old Password" testID="OldPassword" onChangeText={pw => setPassword(pw)} />
+            <PasswordInput text="New Password" testID="NewPassword" onChangeText={pw => setPassword(pw)} />
+            {/* add a button with an api call to change the password */}
+            <Button title='logout' onPress={() => { logout() }} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    generalText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: Colors.primary.RAISIN_BLACK,
+        textAlign: 'center',
+    },
+});
+
+export default SettingsScreen;
