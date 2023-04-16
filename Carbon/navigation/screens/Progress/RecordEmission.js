6c62fe0a1dec237ecf8c015eca9e819affe3f317
Angel ,':) <aq6476@gmail.com>
2023-04-15 21:39:27 -0700

    Misc comments added for clarity

--- Carbon/navigation/screens/Progress/RecordEmission.js
+++ Carbon/navigation/screens/Progress/RecordEmission.js
@@ -9,11 +9,11 @@ export default function RecordEmissionScreen({navigation, route}) {
   
   //Default value for the final submission that we post to the server
   const [emissionsEntry, setEmissionsEntry] = useState({
-        transport_emissions : null, 
-        total_emissions : null,
-        lifestyle_emissions : null, 
-        diet_emissions : null, 
-        home_emissions : null
+        transport_emissions : 0, 
+        total_emissions : 0,
+        lifestyle_emissions : 0, 
+        diet_emissions : 0, 
+        home_emissions : 0
   });
 
   //When we get a "returningEmissionsEntry", which is returned by the category screens, we update our own
@@ -27,13 +27,17 @@ export default function RecordEmissionScreen({navigation, route}) {
   }, [route.params]);
 
 
+  //Posts the results, on a successfull post it will leave the screen
   async function postResults() { 
     try{
+      //for conciseness, emissionsEntry total is just the sum of the others
       let e = emissionsEntry; 
       emissionsEntry.total_emissions = e.diet_emissions + e.home_emissions + e.lifestyle_emissions + e.transport_emissions
 
+      //Check if at least one emission was entered
       if(emissionsEntry.total_emissions === 0 ) throw new Error(`Please Upload at least one Emission Category`);
 
+      //post emission to server
       const response = await fetch(`${API_URL}userEmissions`, {
         method: 'POST',
         headers:{
@@ -42,13 +46,17 @@ export default function RecordEmissionScreen({navigation, route}) {
         },
         body : JSON.stringify(emissionsEntry)
       });
+      //exit screen on successful request
       if(response.status === 200) {
         console.log("Successful Post!");
         navigation.goBack();
       }
+      //if second post for the day - alert and also go back
       else if(response.status === 400){
         throw new Error(`You can only upload results once a day :(`);
+        navigation.goBack(); 
       }
+      //Alert on bad request - should only see on testing 
       else if(response.status === 404){
         throw new Error(`Client ID Not Found`);
       }
@@ -56,6 +64,8 @@ export default function RecordEmissionScreen({navigation, route}) {
       alert(err.message)
     }
   }
+
+
 
 
   return (
