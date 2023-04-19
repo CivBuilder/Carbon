40755138cba7cb636d16b685fcf655a700a91007
Angel ,':) <aq6476@gmail.com>
2023-04-18 19:09:43 -0700

    Added Sustainability Score profile view and Path to the corresponding image.

---
+++ Carbon/util/SustainabilityScoreProfileView.js
@@ -0,0 +1,48 @@
+//Titles for the SustainabilityScores - Server only stores the ints, Client side will translate them to the titles and path into the picture,
+//  Adding A's and An's to avoid future formatting annoyances
+//  NOTE: users MUST provide their own relative path to the Carbon directory
+export const SustainabilityScoreProfileView = [
+    {
+        title : "An Eco Opponent",
+        picture : "assets/rank-icons/rank-icon-1.png"
+    },
+    {
+        title : "A Climate Change Denier",
+        picture : "assets/rank-icons/rank-icon-1.png"
+    },
+    {
+        title : "A Sustainability Saboteur",
+        picture : "assets/rank-icons/rank-icon-2.png"
+    },
+    {
+        title : "A Planet Polluter",
+        picture : "assets/rank-icons/rank-icon-2.png"
+    },
+    
+    {
+        title : "A Carbon Neutral",
+        picture : "assets/rank-icons/rank-icon-3.png"
+    },
+    {
+        title : "A Climate Conscious",
+        picture : "assets/rank-icons/rank-icon-3.png"
+    },
+    {
+        title : "A Renewable Energy Enthusiast",
+        picture : "assets/rank-icons/rank-icon-4.png"
+    },
+    {
+        title : "A Green Leader",
+        picture : "assets/rank-icons/rank-icon-4.png"
+    },
+    {
+        title : "A Sustainability Titan",
+        picture : "assets/rank-icons/rank-icon-5.png"
+    },
+    {
+        title : "An Eco Ambassador",
+        picture : "assets/rank-icons/rank-icon-5.png"
+    }
+]; 
+
+export default {SustainabilityScoreProfileView}; 
