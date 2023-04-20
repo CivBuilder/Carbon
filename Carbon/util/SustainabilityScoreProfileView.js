03cc0e41fd36acd519fc18e48bd12353caecc978
Angel ,':) <75909971+aq6476@users.noreply.github.com>
2023-04-20 10:58:56 -0700

    Update SustainabilityScoreProfileView.js

--- Carbon/util/SustainabilityScoreProfileView.js
+++ Carbon/util/SustainabilityScoreProfileView.js
@@ -1,46 +1,45 @@
 //Titles for the SustainabilityScores - Server only stores the ints, Client side will translate them to the titles and path into the picture,
-//  Adding A's and An's to avoid future formatting annoyances
 //  NOTE: users MUST provide their own relative path to the Carbon directory
 export const SustainabilityScoreProfileView = [
     {
-        title : "An Eco Opponent",
+        title : "Eco Opponent",
         picture : "assets/rank-icons/rank-icon-1.png"
     },
     {
-        title : "A Climate Change Denier",
+        title : "Climate Change Denier",
         picture : "assets/rank-icons/rank-icon-1.png"
     },
     {
-        title : "A Sustainability Saboteur",
+        title : "Sustainability Saboteur",
         picture : "assets/rank-icons/rank-icon-2.png"
     },
     {
-        title : "A Planet Polluter",
+        title : "Planet Polluter",
         picture : "assets/rank-icons/rank-icon-2.png"
     },
     
     {
-        title : "A Carbon Neutral",
+        title : "Carbon Neutral",
         picture : "assets/rank-icons/rank-icon-3.png"
     },
     {
-        title : "A Climate Conscious",
+        title : "Climate Conscious",
         picture : "assets/rank-icons/rank-icon-3.png"
     },
     {
-        title : "A Renewable Energy Enthusiast",
+        title : "Renewable Energy Enthusiast",
         picture : "assets/rank-icons/rank-icon-4.png"
     },
     {
-        title : "A Green Leader",
+        title : "Green Leader",
         picture : "assets/rank-icons/rank-icon-4.png"
     },
     {
-        title : "A Sustainability Titan",
+        title : "Sustainability Titan",
         picture : "assets/rank-icons/rank-icon-5.png"
     },
     {
-        title : "An Eco Ambassador",
+        title : "Eco Ambassador",
         picture : "assets/rank-icons/rank-icon-5.png"
     }
 ]; 
