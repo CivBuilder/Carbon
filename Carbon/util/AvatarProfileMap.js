cb46fd7e42ebf84e73e12982c9f4dd6c052a1700
Angel ,':) <aq6476@gmail.com>
2023-04-20 18:48:48 -0700

    Added mini profile view mapping for avatars

---
+++ Carbon/util/AvatarProfileMap.js
@@ -0,0 +1,9 @@
+export const AvatarView = [
+    "assets/profile-icons/profile-icon-1.png",
+    "assets/profile-icons/profile-icon-2.png",
+    "assets/profile-icons/profile-icon-3.png",
+    "assets/profile-icons/profile-icon-4.png",
+    "assets/profile-icons/profile-icon-5.png",
+    "assets/profile-icons/profile-icon-6.png",
+    "assets/profile-icons/profile-icon-7.png",
+]
