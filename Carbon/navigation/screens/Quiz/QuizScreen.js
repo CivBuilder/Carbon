6890876d88cd5746c4a25bd8ff76ba79546fa390
Chris Catechis <catecc1@unlv.nevada.edu>
2023-04-23 14:42:59 -0700

    clean up code

--- Carbon/navigation/screens/Quiz/QuizScreen.js
+++ Carbon/navigation/screens/Quiz/QuizScreen.js
@@ -14,7 +14,6 @@ const QuizScreen = ({route}) => {
 
     //gets all content from quizcontent
     const fetchData = async() => {
-        console.log('goop');
         console.log("Fetching data for quizcontent");
         try{
         const response = await fetch(API_URL + "quiz/" + route.params.id)
