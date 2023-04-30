8f55505ea67640d07727c08db699abb9cae7fa09
David Weaver <dweaver1017@gmail.com>
2023-04-29 21:13:06 -0700

    add lottie to addgoal

--- Carbon/navigation/screens/Progress/AddGoal.js
+++ Carbon/navigation/screens/Progress/AddGoal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Dimensions, View, Text, SafeAreaView, ScrollView } from 'react-native';
-
+import LottieView from 'lottie-react-native';
 import GoalSetter from './GoalSetter';
 
 const windowWidth = Dimensions.get("window").width;
@@ -10,11 +10,12 @@ const margin = 10;
 export default function AddGoal({ navigation }) {
     return (
         <SafeAreaView style={{ height: windowHeight, backgroundColor: '#F7FCF8' }}>
-            <ScrollView
-                showsHorizontalScrollIndicator={false}
-                style={{ flexGrow: 1 }}
-            >
+            <ScrollView showsHorizontalScrollIndicator={false} style={{ flexGrow: 1 }}>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <LottieView style={{ height: 180, marginVertical: 12, marginBottom: 30 }} source={require('../../../assets/lotties/bikewind.json')} autoPlay loop />
+                </View>
                 <GoalSetter navigation={navigation} />
+                
             </ScrollView>
         </SafeAreaView>
     )
