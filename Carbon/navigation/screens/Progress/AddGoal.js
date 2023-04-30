861ab664121714816d9195a1fdbcdb22274b05b9
David Weaver <dweaver1017@gmail.com>
2023-04-30 00:41:23 -0700

    indentation

--- Carbon/navigation/screens/Progress/AddGoal.js
+++ Carbon/navigation/screens/Progress/AddGoal.js
@@ -12,10 +12,9 @@ export default function AddGoal({ navigation }) {
         <SafeAreaView style={{ height: windowHeight, backgroundColor: '#F7FCF8' }}>
             <ScrollView showsHorizontalScrollIndicator={false} style={{ flexGrow: 1 }}>
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <LottieView style={{ height: 180, marginVertical: 12, marginBottom: 30 }} source={require('../../../assets/lotties/bikewind.json')} autoPlay loop />
-                </View>
-                <GoalSetter navigation={navigation} />
-                
+                <LottieView style={{ height: 180, marginVertical: 12, marginBottom: 30 }} source={require('../../../assets/lotties/bikewind.json')} autoPlay loop />
+            </View>
+            <GoalSetter navigation={navigation} />   
             </ScrollView>
         </SafeAreaView>
     )
