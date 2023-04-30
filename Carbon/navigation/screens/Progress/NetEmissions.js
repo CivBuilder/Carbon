81cb311da3e366ad3378a57d71e5e74306b81922
David Weaver <dweaver1017@gmail.com>
2023-04-29 21:13:33 -0700

    Add lottie to netemission

--- Carbon/navigation/screens/Progress/NetEmissions.js
+++ Carbon/navigation/screens/Progress/NetEmissions.js
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React, {useState, useEffect} from 'react'
 import { getPreviousMonthEmissions, getPreviousMonthLifestyleEmissions } from '../../../util/Goals'
-
+import LottieView from 'lottie-react-native';
 const NetEmissions = ({refreshing, setRefreshing}) => {
   const [lastMonthEmissions, setLastMonthEmissions] = useState(0);
   const [lifestyleEmissions, setLifestyleEmissions] = useState(0);
@@ -23,14 +23,15 @@ const NetEmissions = ({refreshing, setRefreshing}) => {
   }, [initialStart, refreshing, setRefreshing]);
 
   return (
-    <View>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <View style={{marginBottom: 12}}>
         <Text style={{fontSize: 16, fontWeight: '400', textAlign:'center'}}>
           By recycling, you saved <Text style={{fontSize: 20, fontWeight: '500'}}>{`${lifestyleEmissions}`}</Text> lbs of {`CO\u2082`}
         </Text>
         <Text style={{fontSize: 16, fontWeight: '400', textAlign:'center'}}> from the atmosphere last month.</Text>
       </View>
-      <View>
+        <LottieView style={{flex: 1, height: 200, margin: 8}} source={require('../../../assets/lotties/recyclepeople.json')} autoPlay loop />
+      <View style={{marginTop: 12}}>
         <Text style={{fontSize: 16, fontWeight: '400', textAlign:'center'}}>
           Your net emissions is <Text style={{fontSize: 20, fontWeight: '500'}}>{`${lastMonthEmissions - lifestyleEmissions}`}</Text> lbs of {`CO\u2082`} this month.
         </Text>
