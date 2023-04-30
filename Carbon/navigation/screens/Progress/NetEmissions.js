9d31406c0c35f6c89fd21ac7de0be10db026fbc3
Adam Velardi <114274638+adamantisaurus@users.noreply.github.com>
2023-04-30 03:25:39 -0700

    Remove new NetEmissions component
    Will repurpose component into GoalProgress.

--- Carbon/navigation/screens/Progress/NetEmissions.js
+++ Carbon/navigation/screens/Progress/NetEmissions.js
@@ -1,10 +1,6 @@
 import { View, Text } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { getPreviousMonthEmissions, getPreviousMonthLifestyleEmissions } from '../../../util/Goals';
-import { NetEmissionsCSS as styling } from '../../../styling/NetEmissionsCSS';
-import RankProgressBar from '../../../components/ProgressBar';
-import { Ionicons } from '@expo/vector-icons';
-import { Colors } from '../../../styling/Colors';
 
 const NetEmissions = () => {
   const [lastMonthEmissions, setLastMonthEmissions] = useState(0);
@@ -38,32 +34,6 @@ const NetEmissions = () => {
         </Text>
       </View>
     </View>
-
-    /***
-     * NEW VERSION MOCKUP -- DISABLED UNTIL FINISHED AT A LATER TIME (Adam V.)
-    ***/
-    // <View style={styling.netEmissionsGoalContainer}>
-    //   {debugOnTrack !== undefined ?
-    //     <View style={styling.onTrackContainer}>
-    //       <View style={styling.onTrackIcon}>
-    //         {debugOnTrack ? <Ionicons name="checkmark-circle" size={48} color={Colors.secondary.LIGHT_MINT}/>
-    //                       : <Ionicons name="close-circle" size={48} color={Colors.secondary.RED}/>}
-    //       </View>
-    //       <View style={styling.onTrack}>
-    //         <Text style={styling.onTrackText}>You {debugOnTrack ? "are" : "are not"} on track to lowering your emissions this month.</Text>
-    //       </View>
-    //     </View> :
-    //     <></>
-    //   }
-    //   <View style={styling.currentProgress}>
-    //     <RankProgressBar progress={lifestyleEmissions} total={debugGoalValue} barWidth={0.8} />
-    //   </View>
-    //   <View style={styling.netEmissions}>
-    //     <Text style={styling.netEmissionsText}>Your net emissions for this month:</Text>
-    //     <Text style={styling.netEmissionsValue}>{`${lastMonthEmissions - lifestyleEmissions}`}</Text>
-    //     <Text style={styling.units}>{`lbs CO\u2082`}</Text>
-    //   </View>
-    // </View>
   );
 }
 
