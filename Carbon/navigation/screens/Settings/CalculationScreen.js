faf38deb221da9409d70cefc7aa62e43e869b6c7
Miguel Baniqued <bmiguelcedric@gmail.com>
2023-04-30 05:07:46 -0700

    changed 2 in CO2 to subscript

--- Carbon/navigation/screens/Settings/CalculationScreen.js
+++ Carbon/navigation/screens/Settings/CalculationScreen.js
@@ -11,17 +11,17 @@ const CalculationScreen = () => {
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.generalText}>What the values mean</Text>
       <Text style={styles.subheading}>Transport</Text>
-      <Text style={styles.bodyText}>The transport value shows the estimated amount of CO2 emissions from your transportation. This includes emissions from cars, buses, trains, bikes, and airplanes.</Text>
+      <Text style={styles.bodyText}>The transport value shows the estimated amount of CO{`\u2082`} emissions from your transportation. This includes emissions from cars, buses, trains, bikes, and airplanes.</Text>
       <Text style={styles.subheading}>Diet</Text>
-      <Text style={styles.bodyText}>The diet value shows the estimated amount of CO2 emissions from the food you eat. This includes emissions from the production, processing, and transportation of your food.</Text>
+      <Text style={styles.bodyText}>The diet value shows the estimated amount of CO{`\u2082`} emissions from the food you eat. This includes emissions from the production, processing, and transportation of your food.</Text>
       <Text style={styles.subheading}>Home</Text>
-      <Text style={styles.bodyText}>The home value shows the estimated amount of CO2 emissions from your home electricity usage. This includes emissions from the production of electricity, as well as any emissions associated with heating or cooling your home.</Text>
+      <Text style={styles.bodyText}>The home value shows the estimated amount of CO{`\u2082`} emissions from your home electricity usage. This includes emissions from the production of electricity, as well as any emissions associated with heating or cooling your home.</Text>
       <Text style={styles.subheading}>Total Emissions</Text>
-      <Text style={styles.bodyText}>The total emissions value shows the total estimated amount of CO2 emissions saved from your home, diet, and transportation emissions. </Text>
+      <Text style={styles.bodyText}>The total emissions value shows the total estimated amount of CO{`\u2082`} emissions saved from your home, diet, and transportation emissions. </Text>
       <Text style={styles.subheading}>Saved Emissions</Text>
-      <Text style={styles.bodyText}>The saved emissions value shows the estimated amount of CO2 emissions saved from recycling. This includes emissions saved from recycling glass, metal, paper, and plastic </Text>
+      <Text style={styles.bodyText}>The saved emissions value shows the estimated amount of CO{`\u2082`} emissions saved from recycling. This includes emissions saved from recycling glass, metal, paper, and plastic </Text>
       <Text style={styles.subheading}>Net Emissions</Text>
-      <Text style={styles.bodyText}>The net emissions value shows your total estimated amount of CO2 emissions minus your saved emissions.</Text>
+      <Text style={styles.bodyText}>The net emissions value shows your total estimated amount of CO{`\u2082`} emissions minus your saved emissions.</Text>
 
       <Text style={styles.generalText}>How we calculate them</Text>
       <Text style={styles.subheading}>Bike</Text>
